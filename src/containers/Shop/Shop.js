468e1df957fc0b4aaa4ad0b01b4d19d82e594eaa
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-15 23:14:33 -0500

    created base layout for the catalog page

--- src/containers/Shop/Shop.js
+++ src/containers/Shop/Shop.js
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+
+// Styles
+const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 50px 50px 1fr;
+  padding: 0 5%;
+
+  .breadcrumb {
+    grid-column: 1 / 4;
+    grid-row: 1 / 2;
+  }
+
+  .product-filter {
+    grid-column: 1 / 2;
+    grid-row: 2 / 4;
+  }
+
+  .price-sort {
+    grid-column: 2 / 4;
+    grid-row: 2 / 3;
+  }
+
+  .product-card-grid {
+    grid-column: 2 / 4;
+    grid-row: 3 / 4;
+  }
+`;
+
 const Shop = () => {
-  return <main>Shop</main>;
+  return (
+    <Wrapper>
+      <div className="breadcrumb">Breadcrumb</div>
+      <div className="product-filter">Product Filter</div>
+      <div className="price-sort">Price Sort</div>
+      <div className="product-card-grid">Product Card Grid</div>
+    </Wrapper>
+  );
 };
 
 export default Shop;
