516ddacd0db6fb87484b224cca1f55affac21e44
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-16 08:13:27 -0500

    fixed a bug where undefined is passed into the path prop of product card

--- src/components/ProductCardGrid/ProductCardGrid.js
+++ src/components/ProductCardGrid/ProductCardGrid.js
@@ -9,34 +9,41 @@ import productData from "../../productData";
 
 const ProductCardGrid = () => {
   const [data, setData] = useState(productData.hoodies);
+  const [path, setPath] = useState("");
+
   const { filter } = useParams();
 
   useEffect(() => {
     if (!filter) {
       setData(productData.hoodies);
+      setPath("hoodies");
     }
 
     if (filter === "hoodies") {
       setData(productData.hoodies);
+      setPath(filter);
     }
 
     if (filter === "cardigans&sweaters") {
       setData(productData.cardigans);
+      setPath(filter);
     }
 
     if (filter === "jeans") {
       setData(productData.jeans);
+      setPath(filter);
     }
 
     if (filter === "jackets&coats") {
       setData(productData.jackets);
+      setPath(filter);
     }
   }, [filter]);
 
   return (
     <>
       {data.map((item) => (
-        <ProductCard path={filter} id={item.id} name={item.name} price={item.price} img={item.img} key={item.id} />
+        <ProductCard path={path} id={item.id} name={item.name} price={item.price} img={item.img} key={item.id} />
       ))}
     </>
   );
